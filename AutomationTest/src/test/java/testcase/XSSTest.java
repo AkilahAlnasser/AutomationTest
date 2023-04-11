da686b41973a2574fbe195d0639f5453686cd9b2
AkilahAlnasser <a.alnasser6245@student.leedsbeckett.ac.uk>
2023-04-11 11:44:30 +0100

    Test the website

--- AutomationTest/src/test/java/testcase/XSSTest.java
+++ AutomationTest/src/test/java/testcase/XSSTest.java
@@ -1,9 +1,23 @@
 package testcase;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class XSSTest {
+	 WebDriver driver;
+	
   @Test
-  public void f() {
+  public void testXSS() {
+	 
   }
+  @BeforeClass
+  public void open_browser() {
+	  
+	  System.setProperty("webdriver.chrome.driver", ".\\drivers\\chromedriver.exe");
+	  driver =new ChromeDriver();
+	  driver.get("http://localhost/dvwa/");
+  }
+  
 }
